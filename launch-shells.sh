ca2a0a16b7295ebaa1dbe23976f8da1d3737c6fa
Matthew Richardson <M.Richardson@ed.ac.uk>
2014-02-26 21:40:35 +0000

    Change hosts for demo

--- launch-shells.sh
+++ launch-shells.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-./shellinabox 4201 opennebula &
-./shellinabox 4202 w3073 &
-./shellinabox 4203 w3074 &
+./shellinabox 4201 w3290.see.ed.ac.uk &
+./shellinabox 4202 w3290.see.ed.ac.uk &
+./shellinabox 4203 w3295.see.ed.ac.uk &
+./shellinabox 4204 w4017.see.ed.ac.uk &
