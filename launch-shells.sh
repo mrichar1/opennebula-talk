f84261b9480859db99d097a3b043a5a0d6a80c71
Matthew Richardson <M.Richardson@ed.ac.uk>
2014-03-03 11:42:36 +0000

    Change script name.  Add ssh_config

--- launch-shells.sh
+++ launch-shells.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ssh-add
-./shellinabox 4201 w3290.see.ed.ac.uk &
-./shellinabox 4202 w3290.see.ed.ac.uk &
-./shellinabox 4203 w3295.see.ed.ac.uk &
-./shellinabox 4204 w4017.see.ed.ac.uk &
+./shellb 4201 w3290.see.ed.ac.uk &
+./shellb 4202 w3290.see.ed.ac.uk &
+./shellb 4203 w3295.see.ed.ac.uk &
+./shellb 4204 w4017.see.ed.ac.uk &
