605caa94adc90b8b8680be13cd44eff90e6c579f
Matthew Richardson <M.Richardson@ed.ac.uk>
2014-03-03 12:47:40 +0000

    Add an LCFG terminal.  Misc code tidying

--- launch-shells.sh
+++ launch-shells.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ssh-add
-./shellb 4201 w3290.see.ed.ac.uk &
-./shellb 4202 w3290.see.ed.ac.uk &
-./shellb 4203 w3295.see.ed.ac.uk &
-./shellb 4204 w4017.see.ed.ac.uk &
+./shellb 4201 w3290 &
+./shellb 4202 w3290 &
+./shellb 4203 w3295 &
+./shellb 4204 w4017 &
+./shellb 4205 nx.see.ed.ac.uk &
